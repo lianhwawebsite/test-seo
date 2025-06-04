b0ec1a54640e49f51a1cad414135172d3ab9e3f9
ruby.chen <rruby6120tw@gmail.com>
2025-06-04 09:53:44 +0800

    modified data

--- app/(routes)/products/[productId]/page.tsx
+++ app/(routes)/products/[productId]/page.tsx
@@ -10,13 +10,17 @@ export default function Page() {
   return (
     <section className="flex w-full flex-col p-4">
       <div className="flex items-center justify-between mb-4">
-        <h1 className="text-2xl font-bold">{product?.name}</h1>
+        <div>
+          <h1 className="text-2xl font-bold">{product?.name}</h1>
+          <h2>{product?.englishName}</h2>
+        </div>
         <a href="/products" className="border px-4 py-2 rounded-full w-fit">
           Back
         </a>
       </div>
       <article className="flex flex-col w-full border p-4" itemScope itemType="https://schema.org/Product">
         <h2 className="w-full text-xl mb-3">藥品詳情</h2>
+        <p className="w-full">動物藥製字號: {product?.medicineCode}</p>
         <p className="w-full">藥品類別: {product?.type}</p>
         <p className="w-full">
           適用動物:&nbsp;
@@ -27,7 +31,20 @@ export default function Page() {
             </span>
           ))}
         </p>
-        <p className="w-full mb-6">藥品簡介: {product?.description}</p>
+        <p className="w-full">
+          主要成分:&nbsp;
+          {product?.mainIngredients.map((ingredient, index) => (
+            <span key={ingredient} className="w-full">
+              {ingredient}
+              {index < product.mainIngredients.length - 1 && "，"}
+            </span>
+          ))}
+        </p>
+        <p className="w-full">適應症: {product?.indications}</p>
+        <p className="w-full">用法用量: {product?.dosageAndAdministration}</p>
+        <p className="w-full">注意事項: {product?.precautions}</p>
+        <p className="w-full">包裝: {product?.packaging}</p>
+        <p className="w-full mb-6">價格: {product?.price}</p>
       </article>
     </section>
   );
