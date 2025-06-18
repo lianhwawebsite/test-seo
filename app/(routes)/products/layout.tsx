f134619c7dfc25d806245a0c5297ed02b9ac931a
Ruby <rruby6120tw@gmail.com>
2025-06-18 20:35:15 +0800

    feat: modified suspense

--- app/(routes)/products/layout.tsx
+++ app/(routes)/products/layout.tsx
@@ -1,10 +1,13 @@
 import NextBreadcrumb from "@/app/component/NextBreadcrumb";
+import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="px-6 md:px-24 my-10">
-      <NextBreadcrumb />
-      {children}
+      <Suspense>
+        <NextBreadcrumb />
+        {children}
+      </Suspense>
     </div>
   );
 }
