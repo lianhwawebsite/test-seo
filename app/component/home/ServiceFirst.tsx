8526968c64bc32ff68f1b48dae545ed608b85019
Ruby <rruby6120tw@gmail.com>
2025-07-07 19:33:11 +0800

    feat:modified ui

--- app/component/home/ServiceFirst.tsx
+++ app/component/home/ServiceFirst.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const ServiceFirst = ({ title, description, imgSrcPc, imgSrcMo }: { title: string; description: string; imgSrcPc: string; imgSrcMo: string }) => {
   return (
     <div className="flex flex-col items-center gap-8 md:gap-20 lg:gap-28 w-full md:py-10 lg:p-20 text-center">
-      <Image src={imgSrcPc} alt="" width={1440} height={837} className="hidden w-fit md:flex md:w-8/12" />
+      <Image src={imgSrcPc} alt="" width={1440} height={837} className="hidden w-fit md:flex xl:w-8/12 xl:max-w-[1440px]" />
       <Image src={imgSrcMo} alt="" width={1440} height={837} className="w-fit pt-10 px-5 md:hidden" />
       <div className="flex flex-col px-10 pb-10 gap-4 md:gap-9">
         <div className="text-xl md:text-4xl lg:text-5xl font-medium">{title}</div>
