1dde0315df0dd7060f6ad4ab6b147ed971721a81
git <ctrl+p>
2025-08-26 16:54:36 +0800

    feat:modified new url

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,3 @@
-export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://test-seo-beta.vercel.app";
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://test-seo-hazel.vercel.app";
 
 export const abs = (path = "/") => new URL(path, SITE_URL).toString();
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://test-seo-beta.vercel.app",
+  siteUrl: process.env.SITE_URL || "https://test-seo-hazel.vercel.app",
   generateRobotsTxt: true,
   i18n: {
     locales: ["tw", "en"],
